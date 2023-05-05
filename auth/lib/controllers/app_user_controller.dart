8851fa2f753d4947bda816651a6dff9f48e76f17
Sadid-USN <ulamuyaman@gmail.com>
2023-05-05 07:17:08 +0600

    added update username and password method

--- auth/lib/controllers/app_user_controller.dart
+++ auth/lib/controllers/app_user_controller.dart
@@ -6,7 +6,6 @@ import 'package:auth/utils/app_response.dart';
 import 'package:auth/utils/app_utils.dart';
 import 'package:conduit_core/conduit_core.dart';
 
-//! следуйщий Урок 1.14
 class AppUserController extends ResourceController {
   final ManagedContext managedContext;
   AppUserController(this.managedContext);
@@ -34,11 +33,44 @@ class AppUserController extends ResourceController {
   }
 
   @Operation.post()
-  Future<Response> updateProfile() async {
+  Future<Response> updateProfile(
+    @Bind.header(HttpHeaders.authorizationHeader) String header,
+    @Bind.body() User user,
+  ) async {
     try {
-      return AppResponse.ok(message: "Profile updated");
+      final id = AppUtils.getIdFromHeader(header);
+      final fUser = await managedContext.fetchObjectWithID<User>(id);
+      final qUpdateUser = Query<User>(managedContext)
+        ..where((x) => x.id).equalTo(id)
+        ..values.username = user.username ?? fUser?.username
+        ..values.email = user.email ?? fUser?.email;
+      await qUpdateUser.updateOne();
+      final uUser = await managedContext.fetchObjectWithID<User>(id);
+      uUser?.removePropertiesFromBackingMap([
+        AppConst.accessToken,
+        AppConst.refreshToken,
+      ]);
+
+      if (user.username != null && user.username != fUser?.username) {
+        // Имя было изменено
+        return AppResponse.ok(
+            message:
+                "Имя успешно изменено с '${fUser?.username ?? ''}' на '${user.username}'",
+            body: uUser?.backing.contents);
+      } else if (user.email != null && user.email != fUser?.email) {
+        // Email был изменен
+        return AppResponse.ok(
+            message:
+                "Email успешно изменен с '${fUser?.email ?? ''}' на '${user.email}'",
+            body: uUser?.backing.contents);
+      } else {
+        // Ни имя, ни email не были изменены
+        return AppResponse.ok(
+            message: "Данные успешно обновлены", body: uUser?.backing.contents);
+      }
     } catch (error) {
-      return AppResponse.serverError(error);
+      return AppResponse.serverError(error,
+          message: "Ошибка обновления данных");
     }
   }
 
